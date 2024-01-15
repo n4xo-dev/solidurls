79b07c6313f55cd32d277dd1f406365c5c34c5be
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2024-01-15 15:27:27 +0100

    mode: 'planetscale' connection

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -3,10 +3,11 @@ import { migrate } from 'drizzle-orm/mysql2/migrator';
 import { createConnection } from 'mysql2';
 import { drizzle } from 'drizzle-orm/mysql2';
 import { DB_STRING } from '../config';
+import * as schema from './schema';
 
 const connection = createConnection(DB_STRING);
 
-const db = drizzle(connection);
+const db = drizzle(connection, { schema, mode: 'planetscale'});
 
 (async () => {
 await migrate(db, { migrationsFolder: './drizzle' });
--- src/db/planetscale.ts
+++ src/db/planetscale.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/mysql2'
 import { createPool } from 'mysql2'
 import { DB_STRING } from '../config'
+import * as schema from './schema'
 // create the connection
 const connection = createPool(DB_STRING)
 
-export const db = drizzle(connection)
+export const db = drizzle(connection, { schema, mode: 'planetscale' })
